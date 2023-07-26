9a1b81c03153638736e3d5eb53cc6a374b5518ec
Jwhyee <kawnsdud@naver.com>
2023-07-26 17:29:08 +0900

    add : Post entity

---
+++ src/main/java/com/spring/labs/domain/entity/post/Post.java
@@ -0,0 +1,23 @@
+package com.spring.labs.domain.entity.post;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+
+    private String content;
+}
---
+++ src/main/java/com/spring/labs/domain/entity/post/PostRepository.java
@@ -0,0 +1,7 @@
+package com.spring.labs.domain.entity.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostRepository extends JpaRepository<Post, Long> {
+
+}
