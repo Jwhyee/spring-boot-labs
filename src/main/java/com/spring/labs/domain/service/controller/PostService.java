0410448569359099d2b108acf1d6ae6a97ba17e4
Jwhyee <kawnsdud@naver.com>
2023-07-26 17:37:07 +0900

    refactor : Optional throw custom exception

--- src/main/java/com/spring/labs/domain/service/controller/PostService.java
+++ src/main/java/com/spring/labs/domain/service/controller/PostService.java
@@ -2,6 +2,7 @@ package com.spring.labs.domain.service.controller;
 
 import com.spring.labs.domain.entity.post.Post;
 import com.spring.labs.domain.entity.post.PostRepository;
+import com.spring.labs.domain.web.exception.IdNotFoundException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -21,6 +22,6 @@ public class PostService {
 
     public Post findById(Long id) {
         return repository.findById(id)
-                .orElse(null);
+                .orElseThrow(() -> new IdNotFoundException(id + "번 게시물을 찾을 수 없습니다."));
     }
 }
